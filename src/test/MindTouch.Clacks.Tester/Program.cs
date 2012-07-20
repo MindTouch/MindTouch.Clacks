d896dacd9a58a834a224720d38309d298e160747
Arne Claassen <arne@claassen.net>
2012-07-20 14:27:41 -0700

    converted to VS2k10/.NET4.0

--- src/test/MindTouch.Clacks.Tester/Program.cs
+++ src/test/MindTouch.Clacks.Tester/Program.cs
@@ -1,14 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
+﻿/*
+ * MindTouch.Clacks
+ * 
+ * Copyright (C) 2011 Arne F. Claassen
+ * geekblog [at] claassen [dot] net
+ * http://github.com/sdether/MindTouch.Clacks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Text;
 using MindTouch.Clacks.Client;
 using MindTouch.Clacks.Server;
-using Request = MindTouch.Clacks.Client.Request;
-using Response = MindTouch.Clacks.Server.Response;
 
 namespace MindTouch.Clacks.Tester {
     class Program {
